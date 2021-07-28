5b4216d0c7bf301ceb16c1eaa825ea0bc6c20eab
DESKTOP-72LTF07\User <nui.pimcha@gmail.com>
2021-07-28 16:38:06 +0700

    edit1

--- HW04_B1.py
+++ HW04_B1.py
@@ -7,16 +7,16 @@ Created on Tue Jul 27 16:52:31 2021
 
 def largest_number(value):
     listA = []
+    answer = ''
     n = len(str(value)) + 1
     for i in value:
         a = str(i) * n
-        listA.append((a[:n:], i))
-    listA.sort(reverse=True)
+        listA.append((a[:n], i))
+        listA.sort(reverse=True)
     
-    answer = ''
     for i in listA:
         answer = answer + str(i[1])
     return answer
 
-number = [10,1]
+number = [50,2,1,9]
 print('The largest formed number is' , largest_number(number))
